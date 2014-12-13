b39bd3971bef0883bcdd75e40817cf39421bf3a1
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-12-13 13:13:16 -0500

    got a start on the alpha beta function

--- AI.js
+++ AI.js
@@ -1,5 +1,66 @@
 (function() {
-	var AI = function() {};
+	var AI = function() {
+		this.limitDepth= 2;
+		this.currDepth =0;
+		this.myTurn = true;
+		this.alpha = -Infinity;
+		this.beta = Infinity;
+	};
 
 	Chess.AI = new AI();
+
+
+	AI.prototype.alpaBetaMinimax = function(node, alpha, beta) {
+	/*
+	   Returns best score for the player associated with the given node.
+	   Also sets the variable bestMove to the move associated with the
+	   best score at the root node.  
+	*/
+	   // check if at search bound
+	   if (this.limitDepth===this.currDepth)
+	      return node.eval();
+
+	   //check if leaf
+	   var children = node.generateNextStates(true);
+	  
+	   if (children.length === 0){
+	      if (node.state=== Chess.Board.state)//node is root
+	        bestMove = [] 
+	      return node.eval();
+		}
+	   // initialize bestMove
+	   if (node.state=== Chess.Board.state) {//node is root
+	      bestMove = children[0][0];//best move string
+	      //check if there is only one option
+	      if (children.length === 1)
+	         return;
+	   }
+	   if (this.myTurn){
+	   	this.myTurn=false;
+	      for (var child in children){
+	         var result = alphaBetaMinimax(child, alpha, beta)
+	         if (result > alpha){
+	            alpha = result
+	            if (node.state===Chess.board.state)//node is root
+	               bestMove = child[0];
+	         }
+	         if (alpha >= beta)
+	            return alpha
+	      }
+	      return alpha
+
+	   }
+	   else if (!this.myTurn){
+	      for (var child in children)
+	         result = alphaBetaMinimax(child, alpha, beta)
+	         if (result < beta){
+	            beta = result
+	            if (node.state===Chess.board.state)//node is root
+	               bestMove = child[0];
+	         }
+	         if (beta <= alpha)
+	            return beta
+	      return beta
+   		}
+	}
 })();
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Chess-AI
 ========
+A Chess AI made for CMSC404 Final Project in JavaScript.
