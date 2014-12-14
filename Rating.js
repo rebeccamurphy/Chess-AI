8ece2426ae2a4d776341971e9800822b85302e3b
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-12-14 00:35:11 -0500

    fixed up some rating functions

--- Rating.js
+++ Rating.js
@@ -12,12 +12,12 @@
 		counter+= this.rateAttack(Chess.color);
 		counter+= material;
 		counter+= this.rateMovebility(Chess.color);
-		counter+= this.ratePostional(Chess.color, material);
+		counter+= this.ratePostional(Chess.color,moveListLength, depth, material);
 		//for the other team color
-		material=this.rateMaterial(oppColor, moveListLength, depth, )
+		material=this.rateMaterial(oppColor);
 		counter-= this.rateAttack(oppColor);
 		counter-= material;
-		counter-= this.rateMovebility(oppColor);
+		counter-= this.rateMovebility(oppColor, moveListLength, depth, material);
 		counter-= this.ratePostional(oppColor, material);
 		
 		return -(counter+depth*50);
@@ -57,7 +57,7 @@
 	Rating.prototype.rateMovebility= function (color, moveListLength, depth, material){
 		var counter = 0;
 		counter+= moveListLength *5; //5 points per valid move
-		if (moveListLength ===){//current side is in checkmate or stalemate
+		if (moveListLength ===0){//current side is in checkmate or stalemate
 			/*if (!kingSafe){
 				counter-=200000*depth;
 				}
@@ -76,9 +76,10 @@
 			var piece = this.boardState.getPieceAt(i);
 			if (piece !==' '){
 				if (this.boardState.Helpers.getPieceColor(piece)===color){
-					switch(piece.toUpperCase()){
+					
 						//centipawns, no decimals mang
 						if (color===Chess.colors.WHITE){
+							switch(piece.toUpperCase()){
 							case "P": counter+=pawnBoardWhite[i/8][i%8]; 	break;
 							case 'R': counter+=rookBoardWhite[i/8][i%8]; 	break;
 							case 'N': counter+=knightBoardWhite[i/8][i%8]; 	break;
@@ -94,8 +95,10 @@
 									//TODO add a function to board to give the possible moves of the king *30	
 								}
 								break;
+							}
 						}
 						else {
+							switch(piece.toUpperCase()){
 							case "P": counter+=pawnBoardBlack[i/8][i%8]; 	break;
 							case 'R': counter+=rookBoardBlack[i/8][i%8]; 	break;
 							case 'N': counter+=knightBoardBlack[i/8][i%8]; 	break;
@@ -111,11 +114,11 @@
 									//TODO add a function to board to give the possible moves of the king *30	
 								}
 								break;
+							}
 						}
 					}
 				}
 			}
-		}
 		return counter;
 	}
 	Chess.Rating = Rating;
